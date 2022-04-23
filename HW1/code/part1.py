ea332c143bc7c351ae2db1ef43536850b2194bf8
Ron Benchetrit <ronbenc@campus.technion.ac.il>
2022-04-23 12:54:28 +0300

    first commit

---
+++ HW1/code/part1.py
@@ -0,0 +1,17 @@
+def video_to_frames(vid_path: str, start_second, end_second):
+    """
+    Load a video and return its frames from the wanted time range.
+    :param vid_path: video file path.
+    :param start_second: time of first frame to be taken from the
+    video in seconds.
+    :param end_second: time of last frame to be taken from the
+    video in seconds.
+    :return:
+    frame_set: a 4D uint8 np array of size [num_of_frames x H x W x C]
+    containing the wanted video frames in BGR format.
+    """
+ # ====== YOUR CODE: ======
+
+ # ========================
+
+ return frame_set
