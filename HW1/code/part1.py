67add65badc181f77714469621c2cd0c83a07e62
Ron Benchetrit <ronbenc@campus.technion.ac.il>
2022-04-23 21:39:41 +0300

    Implemented 1.b

--- HW1/code/part1.py
+++ HW1/code/part1.py
@@ -18,43 +18,66 @@ def video_to_frames(vid_path: str, start_second, end_second):
     vidCap = cv2.VideoCapture(vid_path)
     H = int(vidCap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     W = int(vidCap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frameCount = int(vidCap.get(cv2.CAP_PROP_FRAME_COUNT))
     FPS = vidCap.get(cv2.CAP_PROP_FPS)
 
-    all_frames_set = np.empty((frameCount, H, W, 3), np.dtype('uint8'))
+    start_frame_idx = int(start_second * FPS)
+    end_frame_idx = int(end_second * FPS)
+    
+    frame_set = np.empty((end_frame_idx - start_frame_idx + 1, H, W, 3), np.dtype('uint8'))
 
     fc = 0
     ret = True
 
-    while (fc < frameCount and ret):
-        ret, all_frames_set[fc] = vidCap.read()
+    while (fc <= end_frame_idx and ret):
+        ret, frame = vidCap.read()
+        if fc >= start_frame_idx:
+            frame_set[fc-start_frame_idx] = frame
         fc += 1
 
     vidCap.release()
 
-    start_frame_idx = int(start_second * FPS)
-    end_frame_idx = int(end_second * FPS)
-    if start_frame_idx == end_frame_idx:
-        frame_set = all_frames_set[start_frame_idx, :, :, :]
-    else:
-        frame_set = all_frames_set[start_frame_idx:end_frame_idx, :, :, :]
-
     # ========================
 
     return frame_set
+
+def gamma_correction(img, gamma):
+    """
+    Perform gamma correction on a grayscale image.
+    :param img: An input grayscale image - ndarray of uint8 type.
+    :param gamma: the gamma parameter for the correction.
+    :return:
+    gamma_img: An output grayscale image after gamma correction -
+    uint8 ndarray of size [H x W x 1].
+    """
+    # ====== YOUR CODE: ======
+    gamma_img = np.power(img, gamma)
+    # ========================
+    return gamma_img
+
+def plot_grayscale_img(grayscale_img, title):
+    plt.title(title)
+    plt.imshow(grayscale_img, 'gray')
+    plt.show()
+
+def plot_grayscale_hist(grayscale_img, title):
+    hist = plt.hist(grayscale_img.ravel(), bins=256, range=[0,255])
+    plt.ylabel('count')
+    plt.xlabel('pixel value')
+    plt.title(title)
+    plt.show()
 
 if __name__ == '__main__':
     vid_path = "HW1\given_data\MilkyChance_StolenDance.mp4"
     start_second = 8
     end_second = 8
     frame_set = video_to_frames(vid_path, start_second, end_second)
-    img = frame_set
+    img = frame_set[0]
     grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    plt.imshow(grayscale_img, 'gray', vmin=0, vmax=255)
-    plt.show()
+    plot_grayscale_img(grayscale_img, title="Grayscale Image")
+    plot_grayscale_hist(grayscale_img, title="Grayscale Histogram")
 
-    hist = plt.hist(grayscale_img.ravel(), bins=256, range=[0,255])
-    plt.ylabel('count')
-    plt.xlabel('pixel value')
-    plt.title('Grayscale Histogram')
-    plt.show()
+    for gamma in [0.5, 1.5]:
+        gamma_corrected_img = gamma_correction(grayscale_img, gamma)
+        plot_grayscale_img(gamma_corrected_img, title="Gamma = " + str(gamma))
+        plot_grayscale_hist(gamma_corrected_img, title="Gamma = " + str(gamma))
+
