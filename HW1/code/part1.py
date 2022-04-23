4b27405d34aac23da4fc0659c80ff3fe3c757c5b
Ron Benchetrit <ronbenc@campus.technion.ac.il>
2022-04-24 01:22:27 +0300

    Implemented part 1.d

--- HW1/code/part1.py
+++ HW1/code/part1.py
@@ -1,3 +1,4 @@
+from statistics import median
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
@@ -54,6 +55,32 @@ def gamma_correction(img, gamma):
     # ========================
     return gamma_img
 
+
+def frames_mean(frame_set):
+    """
+    Calculate the mean frame out of a set of frames.
+    :param frame_set: a 4D uint8 np array of size [num_of_frames x H x W x C].
+    :return:
+    mean_frame: the average image calculated from the set,
+    a 3D uint8 np array of size [H x W x C].
+    """
+    # ====== YOUR CODE: ======
+    mean_frame = np.array(np.mean(frame_set, axis=0), dtype=np.uint8)
+    # ========================
+    return mean_frame
+def frames_median(frame_set):
+    """
+    Calculate the median frame out of a set of frames.
+    :param frame_set: a 4D uint8 np array of size [num_of_frames x H x W x C].
+    :return:
+    median_frame: the median image calculated from the set,
+    a 3D uint8 np array of size [H x W x C].
+    """
+    # ====== YOUR CODE: ======
+    median_frame = np.array(np.median(frame_set, axis=0), dtype=np.uint8)
+    # ========================
+    return median_frame
+
 def plot_grayscale_img(grayscale_img, title):
     plt.title(title)
     plt.imshow(grayscale_img, 'gray')
@@ -65,6 +92,11 @@ def plot_grayscale_hist(grayscale_img, title):
     plt.xlabel('pixel value')
     plt.title(title)
     plt.show()
+
+def plot_color_img(color_img, title):
+    plt.imshow(cv2.cvtColor(color_img, cv2.COLOR_BGR2RGB))
+    plt.title(title)
+    plt.show()      
 
 if __name__ == '__main__':
     vid_path = "HW1\given_data\MilkyChance_StolenDance.mp4"
@@ -84,9 +116,16 @@ if __name__ == '__main__':
 
     start_second = 45
     end_second = 48
-    color_frame_time = np.random.randint(start_second, end_second + 1)
-    frame_set = video_to_frames(vid_path, color_frame_time, color_frame_time)
-    img = frame_set[0]
-    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    plt.title("Selected color image from time section " + str(color_frame_time))
+    frame_set = video_to_frames(vid_path, start_second, end_second)
+    color_frame_idx = np.random.randint(75)
+    img = frame_set[color_frame_idx]
+    plot_color_img(img, title="Selected color image from")
+
+    meam_img = frames_mean(frame_set)
+    median_img = frames_median(frame_set)
+    fig, axs = plt.subplots(1, 2)
+    axs[0].imshow(cv2.cvtColor(meam_img, cv2.COLOR_BGR2RGB))
+    axs[0].set_title("image from the mean")
+    axs[1].imshow(cv2.cvtColor(median_img, cv2.COLOR_BGR2RGB))
+    axs[1].set_title("image from the median")
     plt.show()
