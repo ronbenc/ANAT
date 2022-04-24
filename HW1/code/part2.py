59ab5b84684a124bacb4cabbc8d74d8bfee8129f
Ron Benchetrit <ronbenc@campus.technion.ac.il>
2022-04-24 12:41:20 +0300

    Implemented part 2.b

--- HW1/code/part2.py
+++ HW1/code/part2.py
@@ -1,11 +1,16 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy.fftpack import ifftshift
+
 
 if __name__ == '__main__':
     img_path = "HW1\\my_data\\building.jpg"
     img = cv2.imread(img_path)
-    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    plt.show()
-
+    grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     print(img.dtype)
+
+    dft_img = np.fft.fft2(grayscale_img)
+    shifted_dft_img = np.fft.fftshift(dft_img)
+    plt.imshow(np.log(1 + np.abs(shifted_dft_img)), "gray")
+    plt.show()
