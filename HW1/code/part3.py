d6cf15c5334adf3c93ea4824f8a297aef1ca5715
Ron Benchetrit <ronbenc@campus.technion.ac.il>
2022-04-28 20:22:34 +0300

    Implemented part b

--- HW1/code/part3.py
+++ HW1/code/part3.py
@@ -17,3 +17,29 @@ if __name__ == '__main__':
     axs[0].set_title("Parrot image")
     axs[1].set_title("Selfie image")
     plt.show()
+
+    # section b
+    dft_parrot_img = np.fft.fft2(parrot_img)
+    dft_selfie_img = np.fft.fft2(selfie_img)
+
+    phase_parrot_img = np.angle(dft_parrot_img)
+    phase_selfie_img = np.angle(dft_selfie_img)
+
+    fig, axs = plt.subplots(1, 2)
+    axs[0].imshow(phase_parrot_img, "gray")
+    axs[1].imshow(phase_selfie_img, "gray")
+    axs[0].set_title("Parrot phase image")
+    axs[1].set_title("Selfie phase image")
+    plt.show()
+
+    amp_parrot_img = np.abs(dft_parrot_img)
+    amp_selfie_img = np.abs(dft_selfie_img)
+
+    fig, axs = plt.subplots(1, 2)
+    axs[0].imshow(np.log(1 + np.abs(amp_parrot_img)), "gray")
+    axs[1].imshow(np.log(1 + np.abs(amp_selfie_img)), "gray")
+    axs[0].set_title("Parrot amplitude image")
+    axs[1].set_title("Selfie amplitude image")
+    plt.show()
+
+    # part c
